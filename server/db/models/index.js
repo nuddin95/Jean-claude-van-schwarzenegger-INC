73c1ba051d71f4efd5e5aff7114ede03e715929f
plucker18 <plucker18@gmail.com>
2017-11-02 16:47:55 -0400

    fixed merge conflict in models index

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,12 +1,9 @@
 const User = require('./user')
-<<<<<<< HEAD
 const Reviews = require('./reviews')
-
-=======
 const Product = require('./product')
 const Order = require('./order')
 const OrderProduct = require('./orderProduct')
->>>>>>> master
+
 /**
  * If we had any associations to make, this would be a great place to put them!
  * ex. if we had another model called BlogPost, we might say:
@@ -27,11 +24,8 @@ User.hasMany(Reviews, {as: 'UserId'});
 
 module.exports = {
   User,
-<<<<<<< HEAD
-  Reviews
-=======
+  Reviews,
   Product,
   Order,
   OrderProduct
->>>>>>> master
 }
