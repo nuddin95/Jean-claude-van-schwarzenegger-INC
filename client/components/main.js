2f2a17647ba001fa8d572437adf5068b6ff05838
Plucker <plucker18@gmail.com>
2017-11-04 17:43:33 -0400

    removed one line merge conflict

--- client/components/main.js
+++ client/components/main.js
@@ -17,7 +17,6 @@ const Main = (props) => {
   return (
     <div>
       <h1>Jean-Claude-Schwarzenegger-INC</h1>
-      >>>>>>> master
       <nav>
         {
           isLoggedIn
@@ -37,11 +36,7 @@ const Main = (props) => {
       </nav>
       <hr />
       {children}
-<<<<<<< HEAD
-      <Test />
-=======
       <Test /> // this is for testing front end without messing with this file
->>>>>>> master
     </div>
   )
 }
